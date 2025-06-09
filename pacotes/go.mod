75c30c83895425a4de431985e02dcee5139d17e8
rovicz <roviczzf@gmail.com>
2025-06-08 22:14:31 -0300

    learn: importing a extern package and testing (checkmail package to valideEmails)

--- pacotes/go.mod
+++ pacotes/go.mod
@@ -1,3 +1,5 @@
 module modules
 
 go 1.24.4
+
+require github.com/badoux/checkmail v1.2.4 // indirect
--- pacotes/main.go
+++ pacotes/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"modules/components/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Message bellow:")
 	auxiliar.Write()
+
+	// testando um pacote de validação de e-mails.
+	erro := checkmail.ValidateFormat("contato@victorferreira.dev") // nil (null no golang)
+	erro2 := checkmail.ValidateFormat("1234567890") // invalid format
+	fmt.Println(erro, erro2)
 }
