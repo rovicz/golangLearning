3d902f7232169095ec0b576f4e342db72ce3955a
rovicz <roviczzf@gmail.com>
2025-07-06 18:30:43 -0300

    learn: variatic functions (how works & examples)

--- funcoes-avancadas/main.go
+++ funcoes-avancadas/main.go
@@ -10,6 +10,19 @@ func calcMath(a int, b int) (summ int, sub int) {
 	return // apenas passe um retorno, já que o retorno já foi nomeado e definido.
 }
 
+// função variática.
+func onlySumm(numbers ...int) int { // passando um type seguindo o formato de "...type" declara esse argumento como variável, sendo limitado a UMA variável, sempre como o último argumento da função.
+	total := 0
+
+	// realizando uma soma dentro do loop.
+	for _, number := range numbers {
+		total += number
+	}
+
+	fmt.Println("Os argumentos são:", numbers)
+	return total
+}
+
 func main() {
 	a := 10
 	b := 20
@@ -18,4 +31,8 @@ func main() {
 	summ, sub := calcMath(a, b)
 	fmt.Println("Soma:", summ) // 30
 	fmt.Println("Subtração:", sub) // -10
+
+	// chamando a função variática e passando 10 argumentos.
+	summVariaty := onlySumm(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) // 10 argumentos.
+	fmt.Println("Soma da função variática:", summVariaty) // 55
 }
